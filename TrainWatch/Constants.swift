eadffddad990f56b08ba7004aba915cbe74277d6
Zander Work <zander@zanderwork.com>
2021-04-10 00:32:55 -0700

    basic watch app working

--- TrainWatch/Constants.swift
+++ TrainWatch/Constants.swift
@@ -9,7 +9,7 @@ struct Constants {
     struct Api {
         static let baseUrl = "https://api.wmata.com"
         
-        // this isn't a secret values, more like a client identifier
+        // this isn't a secret value, more like a client identifier
         // making this public isn't a risk
         // pls don't @ me
         static let apiKey = "b83236a40b25404e85f4d2401cc8c08b" // TrainWatch primary key
--- TrainWatch/Views/StationMap/StationMapView.swift
+++ TrainWatch/Views/StationMap/StationMapView.swift
@@ -22,6 +22,8 @@ struct StationMapView: View {
     
     @State private var dataFromApi: Stations? = nil
     
+    @State private var locationManager: CLLocationManager = CLLocationManager()
+    
     func getDataFromApi() {
         DispatchQueue.main.async {
             ApiManager.fetchStations() { stations in
@@ -30,10 +32,15 @@ struct StationMapView: View {
         }
     }
     
+    func requestLocation() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
     var body: some View {
         Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: .constant(.none), annotationItems: dataFromApi?.stations ?? []) { station in
             MapPin(coordinate: station.location, tint: Color(UIColor(name: Constants.Line.abbreviatedMapping[station.lineCode1]!)))
         }
+        .onAppear(perform: requestLocation)
         .onAppear(perform: getDataFromApi)
         .edgesIgnoringSafeArea(.all)
     }
